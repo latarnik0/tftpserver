68adbdf9014ab1d120cfc0d868371e0813092779
latarnik0 <jakub.ryzewski@gmail.com>
2026-08-23 16:51:04 +0200

    Refactored code

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -8,47 +8,74 @@
 #include <cstdlib>
 #include <ctime>
 
-int main() {
-    srand(time(NULL));
+int createSocket()
+{
     int main_socket = socket(AF_INET, SOCK_DGRAM, 0);
-
-    if (main_socket< 0) {
-        std::cerr << "Socket error" << std::endl;
-        return 1;
+    if (main_socket < 0) 
+    {
+        std::cerr << "Socket creation error" << std::endl;
+        exit(EXIT_FAILURE);
     }
     std::cout << "Socket created" << std::endl;
+    return main_socket;
+}
 
+void bindSocket(int main_sock, const sockaddr_in& server_address)
+{
+    if (bind(main_sock, (const sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        std::cerr << "Bind error" << std::endl;
+        close(main_sock);
+        exit(EXIT_FAILURE); 
+    }
+    std::cout << "Bind executed" << std::endl;
+}
+
+sockaddr_in makeServerAddress()
+{
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(MAIN_PORT);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    return server_addr;
+}
 
-    if (bind(main_socket, (const sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Bind error" << std::endl;
-        close(main_socket);
-        return 1;
+bool waitForInitialPacket(int main_socket, char* rx_buffer, sockaddr_in& client_addr, socklen_t& client_addr_len)
+{
+    struct timeval tv_infinite;
+    tv_infinite.tv_sec = 0;  
+    tv_infinite.tv_usec = 0; 
+    
+    setsockopt(main_socket, SOL_SOCKET, SO_RCVTIMEO, &tv_infinite, sizeof(tv_infinite));
+
+    if (recvfrom(main_socket, rx_buffer, RX_BUFFER_SIZE, 0, (sockaddr *)&client_addr, &client_addr_len) < 0) 
+    {
+        std::cerr << "Recvfrom error" << std::endl;
+        return false;
     }
-    std::cout << "Bind executed" << std::endl;
+    return true;
+}
 
-    while (true) {
+int main() 
+{
+    srand(time(NULL));
+    int main_socket = createSocket();
+    struct sockaddr_in server_addr = makeServerAddress();
+    bindSocket(main_socket, server_addr);
+
+    while (true)
+    {
         char rx_buffer[RX_BUFFER_SIZE];
         char tx_buffer[TX_BUFFER_SIZE];
         
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
         memset(&client_addr, 0, sizeof(client_addr));
-        
-        struct timeval tv_infinite;
-        tv_infinite.tv_sec = 0;  
-        tv_infinite.tv_usec = 0; 
-    
-        setsockopt(main_socket, SOL_SOCKET, SO_RCVTIMEO, &tv_infinite, sizeof(tv_infinite));
 
-        if (recvfrom(main_socket, rx_buffer, RX_BUFFER_SIZE, 0, (sockaddr *)&client_addr, &client_addr_len) < 0) {
-            std::cerr << "Recvfrom error" << std::endl;
+        if (!waitForInitialPacket(main_socket, rx_buffer, client_addr, client_addr_len)) {
             close(main_socket);
-            continue;
+            continue; 
         }
 
         char ip_string[ADDRESS_SIZE];
@@ -58,18 +85,22 @@ int main() {
         
         uint16_t op = extractOpcode(rx_buffer);
         
-        if(op == 1){
+        if(op == 1)
+        {
             std::cout << "RRQ" << std::endl;
             sendData(tx_buffer, rx_buffer, client_addr);
         }
-        else if(op == 2){
+        else if(op == 2)
+        {
             std::cout << "WRQ" << std::endl;
             waitForData(rx_buffer, tx_buffer, client_addr, client_addr_len);
         }
-        else{
-            std::cout<<"Illegal TFTP operation"<<std::endl;
+        else
+        {
+            std::cout << "Illegal TFTP operation" << std::endl;
             sendError(main_socket, client_addr, 4, "[Illegal TFTP operation]");
             close(main_socket);
+            break;
         }
     }
     return 0;
